beda2c3e91da42595ceeb0281f69201db14229d1
Zech Zimmerman <zechzimm@gmail.com>
2019-11-21 17:14:41 -0500

    Add method for loading namespaced JSON data

--- beginner/cog.py
+++ beginner/cog.py
@@ -1,7 +1,18 @@
 from discord.ext import commands
+from typing import Any, AnyStr, Union
+import json
+import os.path
 
 
 class Cog(commands.Cog):
     @staticmethod
     def command(*args, **kwargs):
         return commands.command(*args, **kwargs)
+
+    @staticmethod
+    def load_data(namespace: AnyStr) -> Union[Any, None]:
+        path = os.path.join("data", f"{namespace}.json")
+        if os.path.exists(path):
+            with open(path, "r") as json_file:
+                return json.load(json_file)
+        return None
