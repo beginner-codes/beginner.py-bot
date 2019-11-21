a08afba1987ba22aed6b22c8add53bf118adf8c3
Zech Zimmerman <zechzimm@gmail.com>
2019-11-21 16:25:45 -0500

    Move dev commands to dev cog

--- beginner/beginner.py
+++ beginner/beginner.py
@@ -1,7 +1,22 @@
 import os
 import discord
 from discord.ext import commands
+from beginner.cog import Cog
 from functools import lru_cache
+
+
+class BeginnerCog(Cog):
+	@staticmethod
+	@lru_cache()
+	def get_token():
+		if "DISCORD_TOKEN" in os.environ:
+			return os.environ.get("DISCORD_TOKEN")
+		with open("bot.token", "r") as token_file:
+			return token_file.readline().strip()
+
+	@Cog.listener()
+	async def on_ready(self):
+		print("Bot is ready.")
 
 
 @lru_cache()
@@ -17,51 +32,7 @@ def get_client() -> commands.Bot:
 	return client
 
 
-@lru_cache()
-def get_token():
-	if "DISCORD_TOKEN" in os.environ:
-		return os.environ.get("DISCORD_TOKEN")
-	with open("bot.token", "r") as token_file:
-		return token_file.readline().strip()
-
-
 client = get_client()
-
-
-@client.event
-async def on_ready():
-	print("Bot is ready.")
-
-
-@client.command()
-async def load(ctx, extension):
-	if len([r for r in ctx.author.roles if r.id == 644301991832453120]) > 0:
-		client.load_extension(f'cogs.{extension}')
-		print(f"Loaded extension {extension}")
-	else:
-		print(f"Unauthorized attempt to use load() function from {ctx.author.name}.")
-		await ctx.send(f"{ctx.author.name} you don't have permission to perform this action.")
-
-
-@client.command()
-async def unload(ctx, extension):
-	if len([r for r in ctx.author.roles if r.id == 644301991832453120]) > 0:
-		client.unload_extension(f'cogs.{extension}')
-		print(f"Unloaded extension {extension}")
-	else:
-		print(f"Unauthorized attempt to use unload() function from {ctx.author.name}.")
-		await ctx.send(f"{ctx.author.name} you don't have permission to perform this action.")
-
-
-@client.command()
-async def reload(ctx, extension):
-	if len([r for r in ctx.author.roles if r.id == 644301991832453120]) > 0:
-		client.unload_extension(f'cogs.{extension}')
-		client.load_extension(f'cogs.{extension}')
-		print(f"Reloaded extension {extension}")
-	else:
-		print(f"Unauthorized attempt to use reload() function from {ctx.author.name}.")
-		await ctx.send(f"{ctx.author.name} you don't have permission to perform this action.")
 
 
 def load_cogs():
@@ -69,3 +40,9 @@ def load_cogs():
 	client.load_extension('beginner.cogs.help')
 	client.load_extension('beginner.cogs.python')
 	client.load_extension('beginner.cogs.rules')
+
+	if not os.environ.get("PRODUCTION_BOT", False):
+		import beginner.devcog
+		client.add_cog(beginner.devcog.DevCog(client))
+
+	client.add_cog(BeginnerCog(client))
---
+++ beginner/devcog.py
@@ -0,0 +1,44 @@
+from beginner.cog import Cog
+
+
+class DevCog(Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @Cog.listener()
+    async def on_ready(self):
+        print("Dev cog ready.")
+
+    @Cog.command()
+    async def load(self, ctx, *extensions):
+        if extensions and len([r for r in ctx.author.roles if r.id == 644301991832453120]) > 0:
+            for extension in extensions:
+                try:
+                    self.client.load_extension(f'beginner.cogs.{extension}')
+                    print(f"Loaded extension {extension}")
+                except:
+                    print(f"FAILED to load extension {extension}")
+            await ctx.send(f"{ctx.author.display_name} loaded these cogs: {', '.join(extensions)}")
+
+    @Cog.command()
+    async def unload(self, ctx, *extension):
+        if extensions and len([r for r in ctx.author.roles if r.id == 644301991832453120]) > 0:
+            for extension in extensions:
+                try:
+                    self.client.unload_extension(f'beginner.cogs.{extension}')
+                    print(f"Unloaded extension {extension}")
+                except:
+                    print(f"FAILED to unload extension {extension}")
+            await ctx.send(f"{ctx.author.display_name} unloaded these cogs: {', '.join(extensions)}")
+
+    @Cog.command()
+    async def reload(self, ctx, *extensions):
+        if extensions and len([r for r in ctx.author.roles if r.id == 644301991832453120]) > 0:
+            for extension in extensions:
+                try:
+                    self.client.unload_extension(f'beginner.cogs.{extension}')
+                    self.client.load_extension(f'beginner.cogs.{extension}')
+                    print(f"Reloaded extension {extension}")
+                except:
+                    print(f"FAILED to reload extension {extension}")
+            await ctx.send(f"{ctx.author.display_name} reloaded these cogs: {', '.join(extensions)}")
