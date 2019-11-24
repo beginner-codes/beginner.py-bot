cf5d4f8a8e8ee1d3373294a7484a9425133418c3
Zech Zimmerman <zechzimm@gmail.com>
2019-11-23 20:49:19 -0500

    Update rules cog to work with advanced commands

--- beginner/cogs/rules.py
+++ beginner/cogs/rules.py
@@ -1,19 +1,19 @@
 import discord
-import json
-from beginner.cog import Cog
+from beginner.cog import AdvancedCommand, Cog
+from typing import AnyStr, Dict
 
 
 class Rules(Cog):
     def __init__(self, client):
         self.client = client
-        self.rules = self.load_data("rules")
+        self.rules = self.load_data("rules", {"responses": [], "callfunc": "rule"})
 
     @Cog.listener()
     async def on_ready(self):
         print("Rules cog ready.")
 
     @Cog.command()
-    async def rule(self, ctx, *, cmd):
+    async def rule(self, ctx, *args):
         if not self.rules:
             await ctx.send("No rules found in the database, try again later")
             if len([r for r in ctx.author.roles if r.id == 644301991832453120]) > 0:
@@ -21,159 +21,153 @@ class Rules(Cog):
                 import os
 
                 await ctx.send(f">>> {os.getcwd()}\n{os.listdir(os.getcwd())}")
-            return
-
-        found = False
-        if "-add rule" in cmd:  # allow to add a new rule
-            items = cmd.split()
-            alias = items[2]
-            rule = " ".join(items[3:])
-            if len([r for r in ctx.author.roles if r.id == 644301991832453120]) > 0:
-                found = True
-                foundInner = False
-                for r in self.rules["responses"]:
-                    if r["alias"] == alias:
-                        foundInner = True
-                        embedded = discord.Embed(
-                            description=f"A rule with the alias *{alias}* already exists.",
-                            color=0xCC2222,
-                        )
-                        embedded.set_author(
-                            name="Error - existing alias",
-                            icon_url="https://cdn.discordapp.com/icons/644299523686006834/e69f6d4231a6e58eed5884625c4b4931.png",
-                        )
-                        break
-                if foundInner == False:
-                    self.rules["responses"].append({"alias": alias, "text": rule})
-                    file = open("./cogs/rules.json", "w")
-                    file.write(json.dumps(self.rules))
-                    file.close()
-                    embedded = discord.Embed(
-                        description=f"Rules successfully updated with new rule:\n\n**Rule {len(self.rules['responses'])}** - *{alias}*\n{rule}.",
-                        color=0x22CC22,
-                    )
-                    embedded.set_author(
-                        name="Success",
-                        icon_url="https://cdn.discordapp.com/icons/644299523686006834/e69f6d4231a6e58eed5884625c4b4931.png",
-                    )
-
-        elif "-edit rule" in cmd:  # allow to edit an existing rule
-            items = cmd.split()
-            alias = items[2]
-            rule = " ".join(items[3:])
-            if len([r for r in ctx.author.roles if r.id == 644301991832453120]) > 0:
-                found = True
-                foundInner = False
-                for r in self.rules["responses"]:
-                    if r["alias"] == alias:
-                        foundInner = True
-                        r["text"] == rule
-                        file = open("./cogs/rules.json", "w")
-                        file.write(json.dumps(self.rules))
-                        file.close()
-                        embedded = discord.Embed(
-                            description=f"Rule *{alias}* successfully updated to the following:\n{rule}.",
-                            color=0x22CC22,
-                        )
-                        embedded.set_author(
-                            name="Success",
-                            icon_url="https://cdn.discordapp.com/icons/644299523686006834/e69f6d4231a6e58eed5884625c4b4931.png",
-                        )
-                        break
-                if foundInner == False:
-                    embedded = discord.Embed(
-                        description=f"A rule with the alias *{r['alias']}* doesn't exist.",
-                        color=0xCC2222,
-                    )
-                    embedded.set_author(
-                        name="Error - missing alias",
-                        icon_url="https://cdn.discordapp.com/icons/644299523686006834/e69f6d4231a6e58eed5884625c4b4931.png",
-                    )
-
-        elif "-edit alias" in cmd:  # allow to edit an existing rule
-            items = cmd.split()
-            alias1 = items[2]
-            alias2 = items[3]
-            if len([r for r in ctx.author.roles if r.id == 644301991832453120]) > 0:
-                found = True
-                foundInner = False
-                for r in self.rules["responses"]:
-                    if r["alias"] == alias1:
-                        foundInner = True
-                        r["alias"] == alias2
-                        file = open("./cogs/rules.json", "w")
-                        file.write(json.dumps(self.rules))
-                        file.close()
-                        embedded = discord.Embed(
-                            description=f"Rule alias *{alias1}* successfully updated to *{alias2}*.",
-                            color=0x22CC22,
-                        )
-                        embedded.set_author(
-                            name="Success",
-                            icon_url="https://cdn.discordapp.com/icons/644299523686006834/e69f6d4231a6e58eed5884625c4b4931.png",
-                        )
-                        break
-                if foundInner == False:
-                    embedded = discord.Embed(
-                        description=f"A rule with the alias *{r['alias']}* doesn't exist.",
-                        color=0xCC2222,
-                    )
-                    embedded.set_author(
-                        name="Error - missing alias",
-                        icon_url="https://cdn.discordapp.com/icons/644299523686006834/e69f6d4231a6e58eed5884625c4b4931.png",
-                    )
-
         else:
-            try:
-                r = int(cmd)
-                if r < 1 or r > len(self.rules["responses"]):
-                    embedded = discord.Embed(
-                        description=f"There is no rule #{r}. The valid rule numbers are between 1 and {len(self.rules['responses'])}.",
-                        color=0xCC2222,
+            command = AdvancedCommand(self.show_rule)
+            command.add("-add", self.add_rule)
+            command.add("-edit", self.edit_rule)
+            command.add("-edit-alias", self.edit_rule_alias)
+            await command.run(ctx, *args)
+
+    async def add_rule(self, ctx, alias: AnyStr, *rule):
+        if len([r for r in ctx.author.roles if r.id == 644301991832453120]) > 0:
+            error_message = ""
+            error_sub = ""
+            if alias.isnumeric():
+                error_message = f"*{alias}* is an invalid rule alias."
+                error_sub = "invalid alias"
+            elif self.find_rule(alias):
+                error_message = f"A rule with the alias *{alias}* already exists."
+                error_sub = "alias exists"
+            else:
+                text = " ".join(rule)
+                self.rules["responses"].append({"alias": alias, "text": text})
+                self.update_data("rules", self.rules)
+
+                embedded = discord.Embed(
+                    description=f"Rules successfully updated with new rule:\n\n**Rule {len(self.rules['responses'])}** - *{alias}*\n{text}.",
+                    color=0x22CC22,
+                )
+                embedded.set_author(
+                    name="Success",
+                    icon_url="https://cdn.discordapp.com/icons/644299523686006834/e69f6d4231a6e58eed5884625c4b4931.png",
+                )
+                await ctx.send(embed=embedded)
+
+            if error_message:
+                embedded = discord.Embed(
+                    description=error_message,
+                    color=0xCC2222,
+                )
+                embedded.set_author(
+                    name=f"Error - {error_sub}",
+                    icon_url="https://cdn.discordapp.com/icons/644299523686006834/e69f6d4231a6e58eed5884625c4b4931.png",
+                )
+                await ctx.send(embed=embedded)
+
+    async def edit_rule(self, ctx, identifier: AnyStr, *message):
+        if len([r for r in ctx.author.roles if r.id == 644301991832453120]) > 0:
+            rule = self.find_rule(identifier)
+            if rule:
+                rule[1]["text"] = " ".join(message)
+                self.update_data("rules", self.rules)
+
+                embedded = discord.Embed(
+                    description=f"Rule *{rule[1]['alias']}* successfully updated to the following:\n{rule[1]['text']}.",
+                    color=0x22CC22,
+                )
+                embedded.set_author(
+                    name="Success",
+                    icon_url="https://cdn.discordapp.com/icons/644299523686006834/e69f6d4231a6e58eed5884625c4b4931.png",
+                )
+                await ctx.send(embed=embedded)
+            else:
+                embedded = discord.Embed(
+                    description=f"A rule with the alias *{identifier}* doesn't exist.",
+                    color=0xCC2222,
+                )
+                embedded.set_author(
+                    name="Error - missing alias",
+                    icon_url="https://cdn.discordapp.com/icons/644299523686006834/e69f6d4231a6e58eed5884625c4b4931.png",
+                )
+                await ctx.send(embed=embedded)
+
+    async def edit_rule_alias(self, ctx, identifier: AnyStr, alias: AnyStr, *_):
+        if len([r for r in ctx.author.roles if r.id == 644301991832453120]) > 0:
+            rule = self.find_rule(identifier)
+            if rule:
+                rule[1]["alias"] = alias
+                self.update_data("rules", self.rules)
+
+                embedded = discord.Embed(
+                    description=f"Rule *{rule[0]}* alias changed to {alias}.",
+                    color=0x22CC22,
+                )
+                embedded.set_author(
+                    name="Success",
+                    icon_url="https://cdn.discordapp.com/icons/644299523686006834/e69f6d4231a6e58eed5884625c4b4931.png",
+                )
+                await ctx.send(embed=embedded)
+            else:
+                embedded = discord.Embed(
+                    description=f"No rule found for *{identifier}*.",
+                    color=0xCC2222,
+                )
+                embedded.set_author(
+                    name="Error - invalid rule",
+                    icon_url="https://cdn.discordapp.com/icons/644299523686006834/e69f6d4231a6e58eed5884625c4b4931.png",
+                )
+                await ctx.send(embed=embedded)
+
+    async def show_rule(self, ctx, identifier: AnyStr, *_):
+        rule = self.find_rule(identifier)
+        if rule:
+            embedded = discord.Embed(
+                description=rule[1]["text"],
+                color=0x306998,
+            )
+            embedded.set_author(
+                name=f"Rule {rule[0]} - {rule[1]['alias']}",
+                icon_url="https://cdn.discordapp.com/icons/644299523686006834/e69f6d4231a6e58eed5884625c4b4931.png",
+            )
+            await ctx.send(embed=embedded)
+        else:
+            message = f"Invalid rule number. The valid rule numbers are between 1 and 13."
+            if not identifier.isnumeric() and not identifier[1:].isnumeric():
+                aliases = ', '.join(
+                    sorted(
+                        [
+                            _rule["alias"]
+                            for _rule in self.rules["responses"]
+                        ]
                     )
-                    embedded.set_author(
-                        name="Error - incorrect number",
-                        icon_url="https://cdn.discordapp.com/icons/644299523686006834/e69f6d4231a6e58eed5884625c4b4931.png",
-                    )
-                else:
-                    embedded = discord.Embed(
-                        description=self.rules["responses"][r - 1]["text"],
-                        color=0x306998,
-                    )
-                    embedded.set_author(
-                        name=f"Rule {cmd} - {self.rules['responses'][r - 1]['alias']}",
-                        icon_url="https://cdn.discordapp.com/icons/644299523686006834/e69f6d4231a6e58eed5884625c4b4931.png",
-                    )
-            except ValueError:
-                found = False
-                for r in self.rules["responses"]:
-                    if r["alias"] == cmd:
-                        found = True
-                        embedded = discord.Embed(description=r["text"], color=0x306998)
-                        embedded.set_author(
-                            name=f"Rule {self.rules['responses'].index(r)} - {r['alias']}",
-                            icon_url="https://cdn.discordapp.com/icons/644299523686006834/e69f6d4231a6e58eed5884625c4b4931.png",
-                        )
-                if found == False:
-                    text = "*" + cmd + "* is not a valid rule alias. The aliases are:\n"
-                    als = []
-                    for r in self.rules["responses"]:
-                        als.append(r["alias"])
-                    als = sorted(als)
-                    for r in als:
-                        text += r + ", "
-                    text = text[:-2]
-                    embedded = discord.Embed(description=text, color=0xCC2222)
-                    embedded.set_author(
-                        name="Error - incorrect alias",
-                        icon_url="https://cdn.discordapp.com/icons/644299523686006834/e69f6d4231a6e58eed5884625c4b4931.png",
-                    )
-        await ctx.send(embed=embedded)
+                )
+                message = f"*{identifier}* is not a valid rule. The valid rules are:\n{aliases}"
+            embedded = discord.Embed(description=message, color=0xCC2222)
+            embedded.set_author(
+                name="Error - incorrect alias",
+                icon_url="https://cdn.discordapp.com/icons/644299523686006834/e69f6d4231a6e58eed5884625c4b4931.png",
+            )
+            await ctx.send(embed=embedded)
 
     @Cog.command()
     async def aliases(self, ctx):
         als = sorted([rule["alias"] for rule in self.rules["responses"]])
         await ctx.send(f">>> The available rule aliases are:\n{', '.join(als)}")
+
+    def find_rule(self, identifier: AnyStr) -> Dict:
+        rule = None
+        rule_index = -1
+        if identifier.isnumeric():
+            rule_index = int(identifier)
+            if 0 < rule_index <= len(self.rules["responses"]):
+                rule = self.rules["responses"][rule_index - 1]
+        else:
+            rules = self.rules["responses"]
+            for rule_index, _rule in zip(range(1, len(rules) + 1), rules):
+                if _rule["alias"] == identifier.lower():
+                    rule = _rule
+                    break
+        return (rule_index, rule) if rule else None
 
 
 def setup(client):
