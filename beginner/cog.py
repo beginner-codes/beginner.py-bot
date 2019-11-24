b5bc285da1bb167cb3d1392b0512fa1df0a79374
Zech Zimmerman <zechzimm@gmail.com>
2019-11-23 20:47:52 -0500

    Add object for tiddeying up complex commands

--- beginner/cog.py
+++ beginner/cog.py
@@ -1,5 +1,5 @@
 from discord.ext import commands
-from typing import Any, AnyStr, Union
+from typing import Any, AnyStr, Callable, Coroutine, NoReturn, Union
 import json
 import os.path
 
@@ -23,3 +23,24 @@ class Cog(commands.Cog):
     @staticmethod
     def command(*args, **kwargs) -> Any:
         return commands.command(*args, **kwargs)
+
+
+class AdvancedCommand:
+    def __init__(self, default: Coroutine, fail: Union[Coroutine, None]=None):
+        self._default = default
+        self._fail = fail
+        self._options = {}
+
+    def add(self, name: AnyStr, handler: Coroutine) -> "AdvancedCommand":
+        self._options[name.lower()] = handler
+        return self
+
+    async def run(self, ctx, *args):
+        option = args[0].lower() if args else None
+        handler = self._options.get(option, self._default)
+        message = args
+        if option and option in self._options:
+            message = args[1:]
+        elif self._fail:
+            handler = self._fail
+        await handler(ctx, *message)
