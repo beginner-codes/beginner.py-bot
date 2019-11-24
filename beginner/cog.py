3afbe05560ef1ce0da260d281322caca25930a23
Zech Zimmerman <zechzimm@gmail.com>
2019-11-23 20:47:14 -0500

    Add method for updating JSON

--- beginner/cog.py
+++ beginner/cog.py
@@ -6,13 +6,20 @@ import os.path
 
 class Cog(commands.Cog):
     @staticmethod
-    def command(*args, **kwargs):
-        return commands.command(*args, **kwargs)
-
-    @staticmethod
-    def load_data(namespace: AnyStr) -> Union[Any, None]:
+    def load_data(namespace: AnyStr, default: Any=None) -> Union[Any, None]:
+        data = None
         path = os.path.join("data", f"{namespace}.json")
         if os.path.exists(path):
             with open(path, "r") as json_file:
-                return json.load(json_file)
-        return None
+                data = json.load(json_file)
+        return data if data else default
+
+    @staticmethod
+    def update_data(namespace: AnyStr, data: Any):
+        path = os.path.join("data", f"{namespace}.json")
+        with open(path, "w") as json_file:
+            json.dump(data, json_file)
+
+    @staticmethod
+    def command(*args, **kwargs) -> Any:
+        return commands.command(*args, **kwargs)
