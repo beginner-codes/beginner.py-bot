053cc654b5361c170fd20abc54d44fa755ba585e
Zech Zimmerman <zechzimm@gmail.com>
2019-12-08 13:40:25 -0500

    Make startup more verbose

--- beginner/__main__.py
+++ beginner/__main__.py
@@ -1,6 +1,10 @@
 from beginner.beginner import BeginnerCog
 
 
+print("Hello")
 client = BeginnerCog.get_client()
+print("We've got a client")
 BeginnerCog.load_cogs(client)
+print("Cogs loaded")
 client.run(BeginnerCog.get_token())
+print("Good bye")
