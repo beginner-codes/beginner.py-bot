64784506efe268d678419d18c848a2c1cf527af9
Zech Zimmerman <zechzimm@gmail.com>
2019-12-12 11:34:48 -0500

    Update scheduler for tags module add multiple tags

--- beginner/scheduler.py
+++ beginner/scheduler.py
@@ -1,15 +1,13 @@
 from __future__ import annotations
 from beginner.exceptions import BeginnerException
 from beginner.models.scheduler import Scheduler
+from beginner.tags import build_tag_set, fetch_tags
 from collections import defaultdict
 from datetime import datetime, timedelta
-from typing import Any, AnyStr, Callable, Dict, List, Union
+from typing import Any, AnyStr, Callable, Dict, Set, Union
 import asyncio
 import math
 import pickle
-
-
-__tagged_handlers__ = defaultdict(list)
 
 
 def initialize_scheduler():
@@ -26,7 +24,10 @@ def schedule(
     **kwargs,
 ):
     """ Schedule a task to be run and save it to the database. """
-    tag = callback_tag.tag if callable(callback_tag) else callback_tag
+    tags = build_tag_set(callback_tag)  # Get tags into a set
+    # We don't want the "schedule" tag which is required for all tasks
+    if "schedule" in tags:
+        tags.remove("schedule")
     when = datetime.now() + when if isinstance(when, timedelta) else when
     time = _seconds_until_run(when)
     payload = {"args": args, "kwargs": kwargs}
@@ -34,7 +35,7 @@ def schedule(
         raise TaskScheduledForPast(
             f"Task {name} was scheduled for {when} which was {time} seconds ago"
         )
-    task = _schedule_save(name, when, tag, pickle.dumps(payload, 0).decode())
+    task = _schedule_save(name, when, tags, pickle.dumps(payload, 0).decode())
     asyncio.get_event_loop().create_task(_schedule(task, payload))
 
 
@@ -49,9 +50,10 @@ async def _schedule(task: Scheduler, payload: Dict):
 
 
 def _schedule_save(
-    name: AnyStr, when: datetime, tag: AnyStr, payload: AnyStr
+    name: AnyStr, when: datetime, tags: Set, payload: AnyStr
 ) -> Scheduler:
     """ Takes task parameters and creates a Scheduler row in the database. """
+    tag = ",".join(map(str, tags))  # Convert the tag set to a string
     task = Scheduler(name=name, when=when, tag=tag, payload=payload)
     task.save()
     print(f"SCHEDULER: Saved {task.name} for {task.when}")
@@ -65,35 +67,23 @@ def _seconds_until_run(when: datetime) -> int:
 async def _trigger_task(task: Scheduler, payload: Any):
     """ Runs the callbacks tagged for this task and removes the task from the database. """
     try:
-        await _run_tags(task.tag, payload)
+        await _run_tags(set(task.tag.split(",")), payload)
     finally:
         task.delete_instance()
 
 
-async def _run_tags(tag: AnyStr, payload: Dict):
-    """ Runs all callbacks for a given tag. """
-    for callback in __tagged_handlers__[tag]:
+async def _run_tags(tags: Set, payload: Dict):
+    """ Runs all callbacks with the appropriate tags. """
+    for callback in fetch_tags("schedule", tags):
         if asyncio.iscoroutine(callback) or asyncio.iscoroutinefunction(callback):
             await callback(*payload["args"], **payload["kwargs"])
         else:
             callback(*payload["args"], **payload["kwargs"])
 
 
-def tag(callback: Union[AnyStr, Callable]) -> Callable:
-    """ Decorator to tag a callback. """
-
-    def save_tag(callback: Callable, tag: AnyStr = callback) -> Callable:
-        """ Save a callback for a given tag. """
-        __tagged_handlers__[tag].append(callback)
-        callback.tag = tag
-        return callback
-
-    tag = callback
-    if callable(callback):
-        tag = callback.__name__
-        return save_tag(callback, tag)
-    return save_tag
-
-
 class TaskScheduledForPast(BeginnerException):
+    pass
+
+
+class TaskCallbackMissingSchedulerTag(BeginnerException):
     pass
