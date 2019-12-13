8bbcd05adf51a46891c978d5b2139d0831045036
Zech Zimmerman <zechzimm@gmail.com>
2019-12-12 21:09:08 -0500

    Fix very long line

--- bot/beginner/cogs/google.py
+++ bot/beginner/cogs/google.py
@@ -24,7 +24,8 @@ class Google(Cog):
             re = requests.get(
                 url,
                 headers={
-                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36"
+                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                    "Chrome/78.0.3904.97 Safari/537.36"
                 },
             )
             soup = BeautifulSoup(re.content, "html.parser")
