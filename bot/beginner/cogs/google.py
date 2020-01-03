f28e19dc416a2a396dc6137f65440e31f539f232
Zech Zimmerman <zechzimm@gmail.com>
2020-01-02 21:39:13 -0500

    Temp google command fix

--- bot/beginner/cogs/google.py
+++ bot/beginner/cogs/google.py
@@ -1,9 +1,8 @@
 import discord
 from beginner.cog import Cog
-from bs4 import BeautifulSoup
-import requests
-import urllib
-from random import randint
+from googlesearch import search as google
+from urllib.parse import quote_plus
+from random import choice
 
 
 class Google(Cog):
@@ -17,42 +16,34 @@ class Google(Cog):
 
     @Cog.command()  # command decorator inside a cog
     async def google(self, ctx, *, query):
-        results = []
-        queryplus = urllib.parse.quote_plus(query)
-        url = f"https://www.google.com/search?q={queryplus}"
         async with ctx.typing():
-            re = requests.get(
-                url,
-                headers={
-                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
-                    "Chrome/78.0.3904.97 Safari/537.36"
-                },
+            color = choice(self.colors)
+            url_search = (
+                f"https://www.google.com/search?hl=en&q={quote_plus(query)}"
+                "&btnG=Google+Search&tbs=0&safe=on"
             )
-            soup = BeautifulSoup(re.content, "html.parser")
-            res = soup.find("div", {"id": "search"})
+            message = await ctx.send(
+                embed=self.create_google_message(
+                    f"Searching...\n\n[More Results]({url_search})", color
+                )
+            )
+            results = []
+            for result in google("python", num=5, stop=5, safe="on"):
+                short = result[result.find("//") + 2 :].strip("/")
+                if len(short) > 50:
+                    short = f"{short[:50]}..."
+                results.append(f"{len(results) + 1}. [{short}]({result})\n")
+                await message.edit(
+                    embed=self.create_google_message(
+                        f"Results for \"{query}\"\n\n{''.join(results)}\n[More Results]({url_search})",
+                        color,
+                    )
+                )
 
-            rs = res.find_all("div", "r")
-            counter = 0
-            for r in rs:
-                title = r.find("span", "S3Uucc")
-                if title:
-                    link = r.find("a")["href"]
-                    if title.text and link:
-                        results.append(f"[{title.text}]({link})")
-                        counter += 1
-                if counter > 2:
-                    break
-
-            description = "\n".join(results) if results else "*No Results Found*"
-            embedded = discord.Embed(
-                description=f'Results for *"{query}"*\n\n{description}\n\n[See more results]({url})',
-                color=self.colors[randint(0, 3)],
-            )
-            embedded.set_author(
-                name=f"Google Results",
-                icon_url="https://cdn.discordapp.com/icons/644299523686006834/e69f6d4231a6e58eed5884625c4b4931.png",
-            )
-        await ctx.send(embed=embedded)
+    def create_google_message(self, message, color):
+        return discord.Embed(description=message, color=color).set_author(
+            name=f"Google Results", icon_url=self.server.icon_url
+        )
 
 
 def setup(client):
