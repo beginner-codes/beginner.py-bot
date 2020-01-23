f2299d145318de5da03ee18885a0ac94fb870a69
Zech Zimmerman <zechzimm@gmail.com>
2020-01-23 16:19:44 -0500

    Fetch the user skill message everytime it’s needed

--- bot/beginner/cogs/user_roles.py
+++ bot/beginner/cogs/user_roles.py
@@ -7,14 +7,58 @@ class UserRolesCog(Cog):
     def __init__(self, client):
         super().__init__(client)
         self.channel = None
-        self.message = None
         self.reactions_to_roles = {}
 
     @Cog.listener()
     async def on_ready(self):
         logging.debug("User roles is ready")
         self.channel = self.get_channel("role-assignment")
+        message = await self.get_message()
+
+        # Load the roles
+        self.reactions_to_roles = {
+            "beginner": self.get_role("beginners"),
+            "intermediate": self.get_role("intermediates"),
+            "expert": self.get_role("experts"),
+        }
+
+    @Cog.listener()
+    async def on_raw_reaction_add(self, reaction):
+        member = self.server.get_member(reaction.user_id)
+        message = await self.get_message()
+
+        if member.bot:
+            return
+
+        if reaction.message_id != message.id:
+            return
+
+        if reaction.emoji.name not in self.reactions_to_roles:
+            await message.remove_reaction(reaction.emoji, member)
+            return
+
+        await self.assign_members_role(
+            self.reactions_to_roles[reaction.emoji.name], member
+        )
+        await self.remove_reactions(reaction.emoji.name, member)
+        await self.channel.send(
+            f"{member.mention} you've been assigned the role {reaction.emoji.name}",
+            delete_after=10,
+        )
+
+    async def add_reactions(self):
+        message = await self.get_message()
+        await message.add_reaction(self.get_emoji("beginner"))
+        await message.add_reaction(self.get_emoji("intermediate"))
+        await message.add_reaction(self.get_emoji("expert"))
+
+    async def assign_members_role(self, role, member):
+        await member.remove_roles(*self.reactions_to_roles.values())
+        await member.add_roles(role)
+
+    async def get_message(self):
         messages = await self.channel.history(limit=1, oldest_first=True).flatten()
+        message = None
 
         # Load the message from the channel, creating if necessary
         if not messages:
@@ -31,53 +75,18 @@ class UserRolesCog(Cog):
                 text="We reserve the right to adjust your skill level manually if "
                 "we don't feel you've been honest about it."
             )
-            self.message = await self.channel.send(embed=embed)
+            message = await self.channel.send(embed=embed)
             await self.add_reactions()
         else:
-            self.message = messages[0]
+            message = messages[0]
 
-        # Load the roles
-        self.reactions_to_roles = {
-            "beginner": self.get_role("beginners"),
-            "intermediate": self.get_role("intermediates"),
-            "expert": self.get_role("experts"),
-        }
-
-    @Cog.listener()
-    async def on_raw_reaction_add(self, reaction):
-        member = self.server.get_member(reaction.user_id)
-        if member.bot:
-            return
-
-        if reaction.message_id != self.message.id:
-            return
-
-        if reaction.emoji.name not in self.reactions_to_roles:
-            await self.message.remove_reaction(reaction.emoji, member)
-            return
-
-        await self.assign_members_role(
-            self.reactions_to_roles[reaction.emoji.name], member
-        )
-        await self.remove_reactions(reaction.emoji.name, member)
-        await self.channel.send(
-            f"{member.mention} you've been assigned the role {reaction.emoji.name}",
-            delete_after=10,
-        )
-
-    async def add_reactions(self):
-        await self.message.add_reaction(self.get_emoji("beginner"))
-        await self.message.add_reaction(self.get_emoji("intermediate"))
-        await self.message.add_reaction(self.get_emoji("expert"))
-
-    async def assign_members_role(self, role, member):
-        await member.remove_roles(*self.reactions_to_roles.values())
-        await member.add_roles(role)
+        return message
 
     async def remove_reactions(self, keep_reaction, member):
         for emoji in self.reactions_to_roles:
             if emoji != keep_reaction:
-                await self.message.remove_reaction(self.get_emoji(emoji), member)
+                message = await self.get_message()
+                await message.remove_reaction(self.get_emoji(emoji), member)
 
 
 def setup(client):
