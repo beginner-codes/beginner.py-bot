6ad1f82385dade893071b8b527d3b4ac3f3e77a3
Zech Zimmerman <zechzimm@gmail.com>
2020-02-06 15:49:32 -0500

    Improve custom Cog type hints

--- bot/beginner/cog.py
+++ bot/beginner/cog.py
@@ -1,13 +1,15 @@
+from __future__ import annotations
 from beginner.logging import create_logger
 from beginner.tags import TaggableMeta
 from discord.ext import commands
-from typing import Any, AnyStr, Callable, Coroutine, List, NoReturn, Union
+from discord import TextChannel, Client, Guild, Emoji, CategoryChannel, Role
+from typing import Any, AnyStr, Callable, Coroutine, List, Optional, Union
 import json
 import os.path
 
 
 class Cog(commands.Cog, metaclass=TaggableMeta):
-    def __init__(self, client):
+    def __init__(self, client: Client):
         self.client = client
         self.logger = create_logger(self.__class__.__name__)
 
@@ -16,26 +18,28 @@ class Cog(commands.Cog, metaclass=TaggableMeta):
         self.logger.debug("Cog ready")
 
     @property
-    def server(self):
+    def server(self) -> Guild:
         return self.client.get_guild(644299523686006834)
 
-    def get_emoji(self, name: AnyStr, default: Union[Any, None] = None):
+    def get_emoji(self, name: AnyStr, default: Optional[Any] = None) -> Emoji:
         return self.get(self.server.emojis, name, default)
 
-    def get_category(self, name: AnyStr, default: Union[Any, None] = None):
+    def get_category(
+        self, name: AnyStr, default: Optional[Any] = None
+    ) -> CategoryChannel:
         return self.get(self.server.categories, name, default, preserve_case=False)
 
-    def get_channel(self, name: AnyStr, default: Union[Any, None] = None):
+    def get_channel(self, name: AnyStr, default: Optional[Any] = None) -> TextChannel:
         return self.get(self.server.channels, name, default)
 
-    def get_role(self, name: AnyStr, default: Union[Any, None] = None):
+    def get_role(self, name: AnyStr, default: Optional[Any] = None) -> Role:
         return self.get(self.server.roles, name, default, preserve_case=False)
 
     def get(
         self,
         search: List,
         name: AnyStr,
-        default: Union[Any, None] = None,
+        default: Optional[Any] = None,
         preserve_case: bool = True,
     ):
         for element in search:
@@ -45,7 +49,7 @@ class Cog(commands.Cog, metaclass=TaggableMeta):
         return default
 
     @staticmethod
-    def load_data(namespace: AnyStr, default: Any = None) -> Union[Any, None]:
+    def load_data(namespace: AnyStr, default: Any = None) -> Optional[Any]:
         data = None
         path = os.path.join("data", f"{namespace}.json")
         if os.path.exists(path):
@@ -68,12 +72,12 @@ class Cog(commands.Cog, metaclass=TaggableMeta):
 
 
 class AdvancedCommand:
-    def __init__(self, default: Coroutine, fail: Union[Coroutine, None] = None):
+    def __init__(self, default: Coroutine, fail: Optional[Coroutine] = None):
         self._default = default
         self._fail = fail
         self._options = {}
 
-    def add(self, name: AnyStr, handler: Coroutine) -> "AdvancedCommand":
+    def add(self, name: AnyStr, handler: Coroutine) -> AdvancedCommand:
         self._options[name.lower()] = handler
         return self
 
