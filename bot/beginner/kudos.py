512bb1f8100067414e16a3c0808506bc668c78ab
Zech Zimmerman <hi@zech.codes>
2020-05-28 10:27:39 -0400

    Change the Kudos model to a generic Points model

--- bot/beginner/kudos.py
+++ bot/beginner/kudos.py
@@ -1,11 +1,11 @@
-from beginner.models.kudos import Kudos
+from beginner.models.points import Points
 from datetime import datetime
 from typing import List, Tuple
 import peewee
 
 
 def give_user_kudos(kudos: int, user_id: int, giver_id: int, message_id: int):
-    kudos = Kudos(
+    kudos = Points(
         given=datetime.utcnow(),
         user_id=user_id,
         giver_id=giver_id,
@@ -17,8 +17,8 @@ def give_user_kudos(kudos: int, user_id: int, giver_id: int, message_id: int):
 
 def get_user_kudos(user_id) -> int:
     kudos = (
-        Kudos.select(peewee.fn.SUM(Kudos.kudos))
-        .where(Kudos.user_id == user_id)
+        Points.select(peewee.fn.SUM(Points.kudos))
+        .where(Points.user_id == user_id)
         .scalar()
     )
     return 0 if kudos is None else kudos
@@ -26,25 +26,25 @@ def get_user_kudos(user_id) -> int:
 
 def get_highest_kudos(num_users: int) -> List[Tuple[int, int]]:
     return (
-        Kudos.select(Kudos.user_id, peewee.fn.SUM(Kudos.kudos))
+        Points.select(Points.user_id, peewee.fn.SUM(Points.kudos))
         .limit(max(num_users, 1))
-        .group_by(Kudos.user_id)
-        .order_by(peewee.fn.SUM(Kudos.kudos).desc())
+        .group_by(Points.user_id)
+        .order_by(peewee.fn.SUM(Points.kudos).desc())
         .tuples()
     )
 
 
 def remove_kudos(message_id: int, giver_id: int):
-    Kudos.delete().where(
-        Kudos.message_id == message_id, Kudos.giver_id == giver_id
+    Points.delete().where(
+        Points.message_id == message_id, Points.giver_id == giver_id
     ).execute()
 
 
 def get_last_kudos_given(giver_id, user_id):
     row = (
-        Kudos.select(Kudos.given)
-        .where(Kudos.user_id == user_id, Kudos.giver_id == giver_id)
-        .order_by(Kudos.given.desc())
+        Points.select(Points.given)
+        .where(Points.user_id == user_id, Points.giver_id == giver_id)
+        .order_by(Points.given.desc())
         .tuples()
         .get_or_none()
     )
--- bot/beginner/models/kudos.py
+++
@@ -1,9 +0,0 @@
-import beginner.models as models
-
-
-class Kudos(models.Model):
-    given = models.DateTimeField(formats="%Y-%m-%d %H:%M")
-    user_id = models.BigIntegerField()
-    giver_id = models.BigIntegerField()
-    message_id = models.BigIntegerField()
-    kudos = models.IntegerField()
