7004a075fb1c8427f790bac357dcce08e836aab9
Zech Zimmerman <hi@zech.codes>
2020-07-08 21:11:22 -0400

    Add channel management commands

--- beginner/beginner.py
+++ beginner/beginner.py
@@ -141,6 +141,7 @@ class BeginnerCog(Cog):
         BeginnerCog.load_extension(client, "beginner.cogs.help_rotator")
         BeginnerCog.load_extension(client, "beginner.cogs.bump")
         BeginnerCog.load_extension(client, "beginner.cogs.kudos")
+        BeginnerCog.load_extension(client, "beginner.cogs.admin")
         client.add_cog(BeginnerCog(client))
 
     @staticmethod
