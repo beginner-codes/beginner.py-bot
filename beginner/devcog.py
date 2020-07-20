020dc288d9eb22ee3ab7349303e82677b7c21a89
Zech Zimmerman <hi@zech.codes>
2020-07-19 20:26:48 -0400

    Rework dev cog permission system

--- beginner/devcog.py
+++ beginner/devcog.py
@@ -1,55 +1,46 @@
-from beginner.cog import Cog
+from beginner.cog import Cog, commands
 
 
 class DevCog(Cog):
     @Cog.command()
+    @commands.has_guild_permissions(manage_channels=True)
     async def load(self, ctx, *extensions):
-        if (
-            extensions
-            and len([r for r in ctx.author.roles if r.id == 644301991832453120]) > 0
-        ):
-            for extension in extensions:
-                try:
-                    self.client.load_extension(f"beginner.cogs.{extension}")
-                    self.logger.debug(f"Loaded extension {extension}")
-                except:
-                    self.logger.debug(f"FAILED to load extension {extension}")
-            await ctx.send(
-                f"{ctx.author.display_name} loaded these cogs: {', '.join(extensions)}"
-            )
+        for extension in extensions:
+            try:
+                self.client.load_extension(f"beginner.cogs.{extension}")
+                self.logger.debug(f"Loaded extension {extension}")
+            except:
+                self.logger.debug(f"FAILED to load extension {extension}")
+        await ctx.send(
+            f"{ctx.author.display_name} loaded these cogs: {', '.join(extensions)}"
+        )
 
     @Cog.command()
+    @commands.has_guild_permissions(manage_channels=True)
     async def unload(self, ctx, *extensions):
-        if (
-            extensions
-            and len([r for r in ctx.author.roles if r.id == 644301991832453120]) > 0
-        ):
-            for extension in extensions:
-                try:
-                    self.client.unload_extension(f"beginner.cogs.{extension}")
-                    self.logger.debug(f"Unloaded extension {extension}")
-                except:
-                    self.logger.debug(f"FAILED to unload extension {extension}")
-            await ctx.send(
-                f"{ctx.author.display_name} unloaded these cogs: {', '.join(extensions)}"
-            )
+        for extension in extensions:
+            try:
+                self.client.unload_extension(f"beginner.cogs.{extension}")
+                self.logger.debug(f"Unloaded extension {extension}")
+            except:
+                self.logger.debug(f"FAILED to unload extension {extension}")
+        await ctx.send(
+            f"{ctx.author.display_name} unloaded these cogs: {', '.join(extensions)}"
+        )
 
     @Cog.command()
+    @commands.has_guild_permissions(manage_channels=True)
     async def reload(self, ctx, *extensions):
-        if (
-            extensions
-            and len([r for r in ctx.author.roles if r.id == 644301991832453120]) > 0
-        ):
-            for extension in extensions:
-                try:
-                    self.client.unload_extension(f"beginner.cogs.{extension}")
-                    self.client.load_extension(f"beginner.cogs.{extension}")
-                    self.logger.debug(f"Reloaded extension {extension}")
-                except:
-                    self.logger.debug(f"FAILED to reload extension {extension}")
-            await ctx.send(
-                f"{ctx.author.display_name} reloaded these cogs: {', '.join(extensions)}"
-            )
+        for extension in extensions:
+            try:
+                self.client.unload_extension(f"beginner.cogs.{extension}")
+                self.client.load_extension(f"beginner.cogs.{extension}")
+                self.logger.debug(f"Reloaded extension {extension}")
+            except:
+                self.logger.debug(f"FAILED to reload extension {extension}")
+        await ctx.send(
+            f"{ctx.author.display_name} reloaded these cogs: {', '.join(extensions)}"
+        )
 
 
 def setup(client):
