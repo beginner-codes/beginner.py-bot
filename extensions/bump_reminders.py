967bbd409873b1ab304afcdb73d44c99fd77b180
Zech Zimmerman <hi@zech.codes>
2021-02-21 13:24:56 -0500

    Add extension for Discord.me bump reminders

---
+++ extensions/bump_reminders.py
@@ -0,0 +1,33 @@
+import dippy
+import asyncio
+from datetime import datetime, timedelta
+
+
+class BumpReminderExtension(dippy.Extension):
+    client: dippy.Client
+    log: dippy.logging.Logging
+
+    @dippy.Extension.listener("ready")
+    async def setup_reminder(self):
+        self.log.info("Bump reminder setting up")
+        self.client.loop.create_task(self.schedule_next())
+
+    async def send_reminder(self):
+        channel = self.client.get_channel(813085810114297876)
+        role = self.client.get_guild(644299523686006834).get_role(702177774315634788)
+        await channel.send(
+            f"{role.mention} time for the Discord.me bump https://discord.me/dashboard"
+        )
+
+    async def schedule_next(self):
+        bump = self.get_next_bump()
+        self.log.info(f"Scheduling next reminder in {bump} seconds")
+        await asyncio.sleep(bump)
+        self.log.info("Sending reminder")
+        await self.send_reminder()
+        self.client.loop.create_task(self.schedule_next())
+
+    def get_next_bump(self) -> int:
+        bump = datetime.utcnow().replace(minute=0, second=0, microsecond=0)
+        bump += timedelta(hours=6 - bump.hour % 6)
+        return int((bump - datetime.utcnow()).total_seconds())
