ead03acc2a0828ddb9323bc4e205fa8dd4c81a46
Zech Zimmerman <hi@zech.codes>
2021-04-08 15:14:56 -0400

    Update get help instruction message

--- extensions/help_channels/channel_manager.py
+++ extensions/help_channels/channel_manager.py
@@ -79,8 +79,8 @@ class ChannelManager(Injectable):
                 title="Get Help Here",
                 description=(
                     "React with the topic that most closely fits what you need help with. This will claim the channel "
-                    "and move it to the help area.\n\n**Categories**🐍 Python/Discord.py\n🌵 C/C++/C#\n🌎 Web Dev/"
-                    "JavaScript/HTML\n💾 OS/Docker\n🙋 General Help"
+                    "and move it to the help area where you can ask your question.\n\n**Categories**\n🐍 Python/"
+                    "Discord.py\n🌵 C/C++/C#\n🌎 Web Dev/JavaScript/HTML\n💾 OS/Docker/Kubernetes\n🙋 General Help"
                 ),
                 color=0x00FF66,
             )
