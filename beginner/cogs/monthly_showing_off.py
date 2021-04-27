e821215807cdcf53d748fd60568ac765a0f00c92
Zech Zimmerman <hi@zech.codes>
2021-04-26 20:57:01 -0400

    Setup local bot

--- beginner/cogs/monthly_showing_off.py
+++ beginner/cogs/monthly_showing_off.py
@@ -1,10 +1,17 @@
+from beginner.logging import get_logger
 from discord.ext.commands import Cog, Context, command
 
 
 class MonthlyShowingOffCog(Cog):
     """Cog for the monthly showing off challenge!"""
 
-    ...
+    def __init__(self, client):
+        self.client = client
+        self.log = get_logger(("beginner.py", self.__class__.__name__))
+
+    @Cog.listener()
+    async def on_ready(self):
+        self.log.debug(f"{type(self).__name__} is ready")
 
 
 def setup(client):
