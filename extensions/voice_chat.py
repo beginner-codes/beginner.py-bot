1f3a076ebd6e8c934bb42ef7f458ec663035f98c
Zech Zimmerman <hi@zech.codes>
2021-06-12 21:56:45 -0400

    Allow streaming if there's a helper or mod

--- extensions/voice_chat.py
+++ extensions/voice_chat.py
@@ -1,9 +1,10 @@
 from extensions.kudos.manager import KudosManager
-from discord import Member, VoiceState, utils
+from discord import Member, PermissionOverwrite, VoiceChannel, VoiceState, utils
 import dippy
 
 
 class VoiceChatExtension(dippy.Extension):
+    client: dippy.Client
     kudos: KudosManager
 
     @dippy.Extension.listener("voice_state_update")
@@ -20,3 +21,60 @@ class VoiceChatExtension(dippy.Extension):
             and await self.kudos.has_achievement(member, "MUSIC_DJ")
         ):
             await member.add_roles(role)
+
+    @dippy.Extension.listener("voice_state_update")
+    async def manage_streaming_permissions(
+        self, member: Member, before: VoiceState, after: VoiceState
+    ):
+        channel_id = before.channel and before.channel.id or after.channel.id
+        if channel_id != 702221517697581086:
+            return
+
+        if not self.is_voice_mod(member):
+            return
+
+        num_mods = self.get_num_mods()
+        if num_mods:
+            changed = await self.enable_streaming()
+            status = "enabled"
+        else:
+            changed = await self.disable_streaming()
+            status = "disabled"
+
+        if changed:
+            await self.client.get_channel(644828412405481492).send(
+                f"Video streaming has been {status} in {self.client.get_channel(channel_id).mention}"
+            )
+
+    async def disable_streaming(self) -> bool:
+        channel: VoiceChannel = self.client.get_channel(702221517697581086)
+        everyone = utils.get(channel.guild.roles, name="@everyone")
+        overwrites: PermissionOverwrite = channel.overwrites_for(everyone)
+        if overwrites.stream:
+            overwrites.update(stream=False)
+            await channel.edit(
+                reason="Disabling streaming", overwrites={everyone: overwrites}
+            )
+            return True
+        return False
+
+    async def enable_streaming(self) -> bool:
+        channel: VoiceChannel = self.client.get_channel(702221517697581086)
+        everyone = utils.get(channel.guild.roles, name="@everyone")
+        overwrites: PermissionOverwrite = channel.overwrites_for(everyone)
+        if not overwrites.stream:
+            overwrites.update(stream=True)
+            await channel.edit(
+                reason="Disabling streaming", overwrites={everyone: overwrites}
+            )
+            return True
+        return False
+
+    def get_num_mods(self) -> int:
+        channel: VoiceChannel = self.client.get_channel(702221517697581086)
+        return sum(self.is_voice_mod(member) for member in channel.members)
+
+    def is_voice_mod(self, member: Member) -> bool:
+        helpers_role = utils.get(member.guild.roles, name="helpers")
+        mods_role = utils.get(member.guild.roles, name="mods")
+        return helpers_role in member.roles or mods_role in member.roles
