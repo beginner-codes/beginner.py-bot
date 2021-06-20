bc0aec6576517177d441e19cdf0ddc2a72a8570c
Zech Zimmerman <hi@zech.codes>
2021-06-20 14:23:14 -0400

    Move mod extension to a package

--- extensions/mods/commands.py
+++ extensions/mods/commands.py
@@ -2,15 +2,43 @@ from datetime import datetime, timedelta
 from discord import AuditLogAction, Embed, Guild, Member, Message
 from extensions.user_tracking.manager import UserTracker
 import dippy
+import dippy.labels
 
 
 class ModeratorsExtension(dippy.Extension):
     client: dippy.Client
     user_tracking: UserTracker
+    labels: dippy.labels.storage.StorageInterface
 
     @dippy.Extension.listener("ready")
     async def on_ready(self):
         self.client.remove_command("help")
+
+        async def get_label(member: Member, label_name: str, default=None):
+            return await self.labels.get(
+                f"member[{member.guild.id}]", member.id, label_name, default
+            )
+
+        async def set_label(member: Member, label_name: str, value):
+            await self.labels.set(
+                f"member[{member.guild.id}]", member.id, label_name, value
+            )
+
+        def is_admin(member: Member):
+            return member.guild_permissions.administrator
+
+        Member.get_label = get_label
+        Member.set_label = set_label
+        Member.is_admin = is_admin
+
+        async def get_label(guild: Guild, label_name: str, default=None):
+            return await self.labels.get("guild", guild.id, label_name, default)
+
+        async def set_label(guild: Guild, label_name: str, value):
+            await self.labels.set(f"guild", guild.id, label_name, value)
+
+        Guild.get_label = get_label
+        Guild.set_label = set_label
 
     @dippy.Extension.command("!count bans")
     async def cleanup_help_section(self, message: Message):
