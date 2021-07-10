4b116783681d1e8ca8405fecdaf8135af96326e2
Zech Zimmerman <hi@zech.codes>
2021-07-09 20:24:23 -0400

    Get stats command working

--- beginner/cogs/statistics.py
+++ beginner/cogs/statistics.py
@@ -1,25 +1,41 @@
 from beginner.cog import Cog
-from discord import Embed
-from enum import Enum
+from discord import Embed, Status
 
 
 class StatisticsCog(Cog):
+    @Cog.listener()
+    async def on_ready(self):
+        self.logger.info(
+            f"Found {self.get_online():,} online of {self.server.member_count:,} members"
+        )
+
     @Cog.command()
     async def stats(self, ctx):
-        bots = [member for member in self.server.members if member.bot]
-        message = (
-            f"There are currently {self.get_online()} coders online "
-            f"of {self.server.member_count - len(bots)} coders!!!"
-            f"\n\n{self.get_pending()} have not accepted the rules, {len(bots)} bots\n```"
+        num_bots = self.get_bots()
+        await ctx.send(
+            embed=Embed(
+                title=f"Server Stats",
+                description=(
+                    f"There are currently {self.get_online():,} coders online of "
+                    f"{self.server.member_count - num_bots:,} coders!!!\n\n{self.get_pending():,} have not accepted "
+                    f"the rules. Found {num_bots:,} bots."
+                ),
+                color=0x00A35A,
+            )
         )
-        embed = Embed(title=f"Server Stats", description=message, color=0x00A35A)
-        await ctx.send(embed=embed)
+
+    def get_bots(self) -> int:
+        return sum(1 for member in self.server.members if member.bot)
 
     def get_pending(self) -> int:
         return sum(1 for member in self.server.members if member.pending)
 
     def get_online(self) -> int:
-        return sum(1 for member in self.server.members if member.status.online)
+        return sum(
+            1
+            for member in self.server.members
+            if member.status != Status.offline and not member.bot
+        )
 
 
 def setup(client):
