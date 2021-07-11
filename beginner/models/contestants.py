18643944ddb1ccbfc15d04e2112352291b03c805
aryan-debug <52334622+aryan-debug@users.noreply.github.com>
2021-07-11 13:14:32 -0700

    removed datetime as it wasn't needed

--- beginner/models/contestants.py
+++ beginner/models/contestants.py
@@ -4,4 +4,4 @@ import beginner.models as models
 class ContestantInfo(models.Model):
     original_author_id = models.IntegerField()
     bot_message_id = models.IntegerField()
-    datetime = models.DateTimeField(formats="%Y-%m")
+    
