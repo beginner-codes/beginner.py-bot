ac1865233078d98163081931ccaa31f0653a5e55
Zech Zimmerman <hi@zech.codes>
2021-07-25 11:16:02 -0400

    Fix formatting

--- extensions/message_binding.py
+++ extensions/message_binding.py
@@ -64,7 +64,7 @@ class DMBindingExtension(dippy.Extension):
             return False
 
         await channel.send(
-            f"{message}\n\n*From {from_member}",
+            f"{message}\n\n*From {from_member}*",
             allowed_mentions=AllowedMentions(everyone=False, roles=False),
         )
 
