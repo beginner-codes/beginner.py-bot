e756af84e84076c8898af3d1f4eca2158c34121f
Zech Zimmerman <hi@zech.codes>
2021-08-16 20:19:12 -0400

    Add minecraft command

---
+++ extensions/minecraft.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+import asyncio
+import dippy
+import dippy.labels
+import discord
+
+
+class MinecraftExtension(dippy.Extension):
+    client: dippy.Client
+    log: dippy.logging.Logging
+    labels: dippy.labels.storage.StorageInterface
+
+    @dippy.Extension.command("!minecraft")
+    async def minecraft_command(self, message: discord.Message):
+        if message.channel.id != 834200603474657321:
+            return
+
+        await message.channel.send(
+            f"```\nJava Edition: mc.beginnerpy.com\nBedrock: mc.beginnerpy.com:8152\n```\n**Rules**\n- Mods that give "
+            f"unfair advantage are not allowed.\n- If your mods or custom client get you banned you may not be allowed "
+            f"back.\n- Griefing and such are not allowed."
+        )
