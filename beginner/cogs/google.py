cd5fef472ebefa535f927448ffa8d49fb50c5fc6
Zech Zimmerman <hi@zech.codes>
2021-10-04 15:24:08 -0400

    Remove typing indicator

--- beginner/cogs/google.py
+++ beginner/cogs/google.py
@@ -20,35 +20,25 @@ class Google(Cog):
     @Cog.command()  # command decorator inside a cog
     async def google(self, ctx, *, query):
         google_settings = beginner.config.scope_getter("google")
-        async with ctx.typing():
-            color = choice(self.colors)
-            url_search = (
-                f"https://www.google.com/search?hl=en&q={quote_plus(query)}"
-                "&btnG=Google+Search&tbs=0&safe=on"
+        color = choice(self.colors)
+        url_search = f"https://www.google.com/search?hl=en&q={quote_plus(query)}" "&btnG=Google+Search&tbs=0&safe=on"
+        message = await ctx.send(
+            embed=self.create_google_message(f"Searching...\n\n[More Results]({url_search})", color)
+        )
+        query_obj = google(
+            "customsearch",
+            "v1",
+            developerKey=google_settings("custom_search_key", env_name="GOOGLE_CUSTOM_SEARCH_KEY"),
+        )
+        query_result = (
+            query_obj.cse()
+            .list(
+                q=query,
+                cx=google_settings("custom_search_engine", env_name="GOOGLE_CUSTOM_SEARCH_ENGINE"),
+                num=5,
             )
-            message = await ctx.send(
-                embed=self.create_google_message(
-                    f"Searching...\n\n[More Results]({url_search})", color
-                )
-            )
-            query_obj = google(
-                "customsearch",
-                "v1",
-                developerKey=google_settings(
-                    "custom_search_key", env_name="GOOGLE_CUSTOM_SEARCH_KEY"
-                ),
-            )
-            query_result = (
-                query_obj.cse()
-                .list(
-                    q=query,
-                    cx=google_settings(
-                        "custom_search_engine", env_name="GOOGLE_CUSTOM_SEARCH_ENGINE"
-                    ),
-                    num=5,
-                )
-                .execute()
-            )
+            .execute()
+        )
 
         results = []
         for result in query_result.get("items", []):
