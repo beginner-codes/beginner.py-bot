290ca19b2ca69cc0eec01c1aa71bcc940f1929e8
Zech Zimmerman <hi@zech.codes>
2021-10-05 14:33:34 -0400

    Remove extra print

--- beginner/runner.py
+++ beginner/runner.py
@@ -110,7 +110,6 @@ class Executer:
     def importer(self, name, *args, **kwargs):
         if self.imported_module_parser(name) not in self.import_whitelist:
             raise ImportError(f"Module is not whitelisted: {name}")
-        print(name)
         if name == "numpy":
             return Numpy()
         return __import__(name, *args, **kwargs)
